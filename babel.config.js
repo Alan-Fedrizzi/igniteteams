de590b523f0052fa6ef89fb9c4b5f9f68e25cf8f
alanfedrizzi <alanfedrizzi@gmail.com>
2024-05-17 16:38:03 +0200

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@assets": "./src/assets",
+            "@components": "./scr/components",
+            "@routes": "./scr/routes",
+            "@screens": "./scr/screens",
+            "@storage": "./scr/storage",
+            "@theme": "./scr/theme",
+            "@utils": "./scr/utils",
+          },
+        },
+      ],
+    ],
   };
 };
